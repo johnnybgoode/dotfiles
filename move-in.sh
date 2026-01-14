da725b60e0e2d4449e254b312a56ee287aeae309
John Entwistle <j.entwistle76@gmail.com>
2026-01-14 01:21:50 -0500

    Add ghostty conf

--- move-in.sh
+++ move-in.sh
@@ -2,7 +2,9 @@
 
 ROOT_DIR="$(cd "$(dirname "$0")" &> /dev/null && pwd -P)"
 
-for file in $(ls "$ROOT_DIR" | grep -v "move-in"); do
+for file in $(ls "$ROOT_DIR" | grep -v "move-in" | grep -v "ghostty.conf"); do
   echo "Linking '$file'..."
   ln -ns "$ROOT_DIR/$file" "$HOME/.$file" 
 done
+
+mkdir -p "$HOME/.config/ghostty" && ln -s "$ROOT_DIR/ghostty.conf" "$HOME/.config/ghostty/config"
